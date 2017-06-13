56f5db3bc08f9b53b43c821f37b1686a24e596c0
Peter Melchior <peter.m.melchior@gmail.com>
2017-06-13 15:13:21 +0200

    first stab at nmf with new glmm interface

--- proxmin/nmf.py
+++ proxmin/nmf.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 import logging
 import numpy as np
+from . import utils
 
 logging.basicConfig()
 logger = logging.getLogger("proxmin.nmf")
@@ -23,6 +24,19 @@ def prox_likelihood_A(A, step, S=None, Y=None, prox_g=None, W=1):
 def prox_likelihood_S(S, step, A=None, Y=None, prox_g=None, W=1):
     return prox_g(S - step*grad_likelihood_S(S, A, Y, W=W), step)
 
+def prox_likelihood(X, step, Xs=None, j=None, Y=None, W=None, prox_g_S=prox_id, prox_g_A=prox_id):
+    if j == 0:
+        return prox_likelihood_A(X, step, S=Xs[1], Y=Y, prox_g=prox_g_A, W=W)
+    else:
+        return prox_likelihood_S(X, step, A=Xs[0], Y=Y, prox_g=prox_g_S, W=W)
+
+def steps_AS(Xs=None, j=None, Wmax=1):
+    if j == 0:
+        L = utils.get_spectral_norm(Xs[1].T) * Wmax  # ||S*S.T||
+    else:
+        L = utils.get_spectral_norm(Xs[0]) * Wmax # ||A.T * A||
+    return 1./L
+
 # split X into K components along axis
 # apply prox_list[k] to each component k
 # stack results to reconstruct shape of X
@@ -39,83 +53,49 @@ def prox_components(X, step, prox_list=[], axis=0):
         Pk = [prox_list[k](X[:,k], step[k]) for k in range(K)]
     return np.stack(Pk, axis=axis)
 
-def dot_components(C, X, axis=0, transpose=False):
-    assert X.shape[axis] == len(C)
-    K = X.shape[axis]
 
-    if axis == 0:
-        if not transpose:
-            CX = [C[k].dot(X[k]) for k in range(K)]
-        else:
-            CX = [C[k].T.dot(X[k]) for k in range(K)]
-    if axis == 1:
-        if not transpose:
-            CX = [C[k].dot(X[:,k]) for k in range(K)]
-        else:
-            CX = [C[k].T.dot(X[:,k]) for k in range(K)]
-    return np.stack(CX, axis=axis)
-
-
-def nmf(Y, A0, S0, prox_A, prox_S, prox_S2=None, M2=None, lM2=None,
-        max_iter=1000, W=None, e_rel=1e-3, algorithm='ADMM',
-        outer_max_iter=50, min_iter=10):
-
-    K = S0.shape[0]
-    A = A0.copy()
-    S = S0.copy()
-    S_ = S0.copy() # needed for convergence test
-    beta = 1. # 0.75    # TODO: unclear how to chose 0 < beta <= 1
+def nmf(Y, A0, S0, proxs_A=None, proxs_S=None, W=None, Ls=None, l0_thresh=None, l1_thresh=None, max_iter=1000, min_iter=10, e_rel=1e-3, traceback=False):
 
     if W is not None:
-        W_max = W.max()
+        Wmax = W.max()
     else:
-        W = W_max = 1
+        W = Wmax = 1
 
-    all_errors = []
-    all_norms = []
-    for it in range(outer_max_iter):
-        # A: simple gradient method; need to rebind S each time
-        prox_like_A = partial(prox_likelihood_A, S=S, Y=Y, prox_g=prox_A, W=W)
-        step_A = beta**it / lipschitz_const(S) / W_max
-        it_A = APGM(A, prox_like_A, step_A, max_iter=max_iter)
+    from functools import partial
+    f = partial(prox_likelihood, Y=Y, W=W, prox_g_S=prox_g_S)
+    steps_f = partial(steps_AS, Wmax=Wmax)
 
-        # S: either gradient or ADMM, depending on additional constraints
-        prox_like_S = partial(prox_likelihood_S, A=A, Y=Y, prox_g=prox_S, W=W)
-        step_S = beta**it / lipschitz_const(A) / W_max
-        if prox_S2 is None or algorithm == "APGM":
-            it_S = APGM(S_, prox_like_S, step_S, max_iter=max_iter)
-            errors = []
-        elif algorithm == "ADMM":
-            # steps set to upper limit per component
-            step_S2 = step_S * lM2
-            it_S, S_, _, _, errors = ADMM(S_, prox_like_S, step_S, prox_S2, step_S2, A=M2,
-                                          max_iter=max_iter, e_rel=e_rel)
-        elif algorithm == "SDMM":
-            # TODO: Check that we are properly setting the step size.
-            # Currently I am just using the same form as ADMM, with a slightly modified
-            # lM2 in nmf_deblender
-            step_S2 = step_S * lM2
-            it_S, S_, _, _, errors = SDMM(S_, prox_like_S, step_S, prox_S2, step_S2,
-                                          constraints=M2, max_iter=max_iter, e_rel=e_rel)
+    # sanitize and merge proxs_A and proxs_S:
+    # require at least non-negativity
+    proxs_g = []
+    if proxs_A is None:
+        proxs_g.append([prox_plus])
+    else:
+        proxs_g.append(proxs_A)
+    if proxs_S is None:
+        # S: non-negativity or L0/L1 sparsity plus ...
+        if l0_thresh is None and l1_thresh is None:
+            proxs_g.append([prox_plus])
         else:
-            raise Exception("Unexpected 'algorithm' to be 'APGM', 'ADMM', or 'SDMM'")
+            # L0 has preference
+            if l0_thresh is not None:
+                if l1_thresh is not None:
+                    logger.warn("Warning: l1_thresh ignored in favor of l0_thresh")
+                prox_S = partial(prox_hard, l=l0_thresh)
+            else:
+                prox_S = partial(prox_soft_plus, l=l1_thresh)
+    else:
+        proxs_g.append(proxs_S)
 
-        logger.info("{0} {1} {2} {3} {4} {5}".format(it, step_A, it_A, step_S, it_S,
-                                                     [(S[i,:] > 0).sum()for i in range(S.shape[0])]))
+    # set step sizes and Ls to None as default
+    steps_g = [[None] * len(proxs_g[j]) for j in range(2)]
+    if Ls is None:
+        Ls = [[None] * len(proxs_g[j]) for j in range(2)]
 
-        if it_A == 0 and it_S == 0:
-            break
+    Xs = [A0.copy(), S0.copy()]
+    res = pa.glmm(Xs, f, steps_f, proxs_g, steps_g, Ls=Ls, max_iter=max_iter, e_rel=e_rel, traceback=traceback)
 
-        ## Convergence crit from Langville 2014, section 5 ?
-        NMF_converge, norms = check_NMF_convergence(it, S_, S, e_rel, K, min_iter)
-        all_errors += errors
-        all_norms.append(norms)
-
-        # Store norms and errors
-
-        if NMF_converge:
-            break
-
-        S[:,:] = S_[:,:]
-    S[:,:] = S_[:,:]
-    return A,S, [all_norms, all_errors]
+    if not traceback:
+        return res[0], res[1]
+    else:
+        return res[0][0], res[0][1], res[1]
