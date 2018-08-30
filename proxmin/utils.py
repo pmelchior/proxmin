4887e49bc183eac83982ba71c97318035568c5fa
Peter Melchior <peter.m.melchior@gmail.com>
2018-08-30 11:46:44 -0400

    docstring update

--- proxmin/utils.py
+++ proxmin/utils.py
@@ -184,12 +184,10 @@ class Traceback(object):
                     self._store_variable(j, k, m, v[m])
 
 class ApproximateCache(object):
-    """Cache large arrays that don't change much
+    """Cache function evaluations that don't change much
 
-    Certain calculations, like `~proxmin.nmf.Steps_AS._one_over_lipschitzA`
-    take a long time to calculate. But after a few iterations, when the
-    matrices begin to converge, changes in the Lipschitz constant are small
-    and these functions become an unnecessary time sync.
+    Certain calculations take a long time but have little change after a few
+    iterations, so that these functions become an unnecessary time sink.
     This class allows the user to store these (and similar) values and
     only calculate them when there is a substantial change in the value.
 
